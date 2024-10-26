037cc3a4a48a4acbd3c1068dac7ba73d3963ce88
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-27 01:04:33 +0530

    ..

--- Task2/index.js
+++ Task2/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./db/connectDB.js";
+import router from "./routes/route.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = 3000;
 app.get('/health',(req,res) => {
     res.status(200).json({message:"db is connected"});
 });
+app.use("/api/v1",router);
 connectDB();
 
 app.listen(PORT ,()=> {
