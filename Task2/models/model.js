4f52f16ea54bf336239756f5ed8e16b35f8abf62
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-27 01:04:43 +0530

    ..

---
+++ Task2/models/model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true,
+        trim:true,
+
+    },
+    password: {
+        type: String,
+        required: true,
+        trim:true,
+    }
+})
+export const User = mongoose.model('User',UserSchema);
