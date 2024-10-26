a7e1c45d4a4a7705a2a532f3a865493c16964fe1
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-27 01:04:48 +0530

    ..

--- Task2/routes/route.js
+++ Task2/routes/route.js
@@ -1,1 +1,9 @@
-const express =require('require');
+import express from 'express';
+import { createUser } from '../controllers/constrol.js';
+
+const router =express.Router();
+
+
+router.route('/users').post(createUser);
+
+export default router;
