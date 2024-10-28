7afbb14dd2f1ca09941d268caf8ee0860089bad2
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-10-28 17:00:58 +0530

    ..

--- Task3/index.js
+++ Task3/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB} from "./db/connectDB.js";
-import router from './routes/route.js';
+const router =express.Router();
 
 dotenv.config();
 const app =express()
