71b084cea7ef190a9e1eb5d7ad2e3a9c5a2ce4c5
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-06 23:51:36 +0530

    db

--- Task3/db/connectDB.js
+++ Task3/db/connectDB.js
@@ -6,6 +6,7 @@ dotenv.config();
 export const connectDB =async () => {
     try{
         const mongoUri = process.env.MONGO_URI;
+        console.log("Mongo URI:", mongoUri);
         await mongoose.connect(mongoUri);
         console.log("MongoDB connected");
 
