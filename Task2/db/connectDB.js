e9ab26433fb93f9a54cb408fbce4b842fac67235
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-17 17:55:35 +0530

    ..

--- Task2/db/connectDB.js
+++ Task2/db/connectDB.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 export const connectDB =async () => {
     try{
         const mongoUri=process.env.MONGO_URI;
-        await mongoose.connect(mongoUri);
-        console.log('MongiDb is connected');
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDb is connected');
     }catch(error){
-        console.log(error);
+        console.error("MongoDB connection error:", error.message);
     }
 };
 
