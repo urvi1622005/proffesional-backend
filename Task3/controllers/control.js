71c12791e0c487e7bd315315deb3b11bf78a537e
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-17 17:55:47 +0530

    ..

--- Task3/controllers/control.js
+++ Task3/controllers/control.js
@@ -1,13 +1,26 @@
-import Video from '../models/model.js'
+import Video from '../models/model.js';
 
-export const createVideo =async (req,res) => {
-    try {
-        const{title};
-        const{description};
-        const{url};
-        const Video =newVideo({title,description,url});
+export const createVideo = async (req, res) => {
+  try {
+    const { title, description, url } = req.body; // Destructure all fields in a single line
 
-        res.status(200).json({message:"created"})
-    }
-    
-}
+    // Create a new video instance
+    const newVideo = new Video({ title, description, url });
+
+    // Save the new video to the database
+    await newVideo.save();
+
+    // Respond with a success message and the created video data
+    res.status(201).json({
+      message: "Video created successfully",
+      video: newVideo,
+    });
+  } catch (error) {
+    // Catch any errors and return a 500 status with the error message
+    console.error(error);
+    res.status(500).json({
+      message: "Error creating video",
+      error: error.message,
+    });
+  }
+};
