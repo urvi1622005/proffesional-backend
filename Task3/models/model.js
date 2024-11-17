6b075a0917b84eb88d20db710c1c4d7acff6ca23
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-11-17 17:55:53 +0530

    ..

---
+++ Task3/models/model.js
@@ -0,0 +1,20 @@
+import mongoose from mongoose ;
+
+const UserSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    }
+})
+
+export const Video = mongoose.model('Video', UserSchema);
+
+    
