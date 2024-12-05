6fff6303070d0a8347cbaccaa0aa9c27d6cf4ce2
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-06 00:41:38 +0530

    ..

--- Linkyfy/src/App.jsx
+++ Linkyfy/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Chat from '../src/Components/Chat-UI';
 import Sidebar from './components/Sidebar';
+import Navbar from './Components/Navbar';
+import Login from './Components/login';
+import { motion } from 'framer-motion';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import Message from './Components/MessageInput';
 import '../src/main.css';
 
 const App = () => {
@@ -16,7 +21,12 @@ const App = () => {
 
   return (
     <div className="app-container flex">
+      <Navbar />
       <Sidebar />
+      <Chat/>
+      <Login/>
+      <Message/>
+      
       {user ? <Chat user={user} /> : <div>Loading...</div>}
     </div>
   );
