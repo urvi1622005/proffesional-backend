c9a9d2c513a5df7872584c4e449ea8604e2b2154
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-12 07:39:28 +0530

    .....

---
+++ Blog/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from mongoose;
+import dotenv from dotenv;
+
+dotenv.config();
+
+export const connectDb =async () => {
+    try{
+        const mongoUri = process.env.MONGO_URI;
+        console.log("Mongo URI:", mongoUri);
+        await mongoose.connect(mongoUri);
+        console.log("MongoDB connected");
+
+
+    }catch(error){
+        console.error(error);
+    }       
+}
+       
