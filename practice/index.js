7ce39b82ba54edfcc2e7082112e7bb027972d54e
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-03 22:27:44 +0530

    ...

--- practice/index.js
+++ practice/index.js
@@ -1,15 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDb } from '../Blog/db/connectDb';
-const Router =express.Router();
- 
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db/connectDb.js");
+
 dotenv.config();
+const app = express();
+const PORT = 3000;
 
-const app=express();
-const PORT=3000;
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-app.listen(3000,()=> {
+app.use(express.json());
 
-})
+
+app.get("/video", (req, res) => {
+  res.status(200).json({ message: "db is connected" });
+});
+
+
+app.post("/video", (req, res) => {
+  const { title, description, url } = req.body;
+
+  
+  if (!title || !description || !url) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+
+  res.status(201).json({
+    message: "Video created successfully",
+    data: { title, description, url },
+  });
+});
+
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
