b818ad506e5bb29ffa836e34f3033913f099466d
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-04 18:06:16 +0530

    ..

--- practice/middlewares/auth.js
+++ practice/middlewares/auth.js
@@ -9,7 +9,7 @@ export const authorization = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.id; // Attach user ID to request object
+    req.user = decoded.id; 
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid or expired token." });
