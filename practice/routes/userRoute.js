b2dd444fcc6d3eeff1be7f2a28c03e4e0055bfce
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-04 14:57:13 +0530

    ..

--- practice/routes/userRoute.js
+++ practice/routes/userRoute.js
@@ -1,28 +1,9 @@
 import express from "express";
-import { User } from "../models/User.js";
+import { registerUser, loginUser } from "../controllers/user.js";
 
-const router = express.Router();
+const userRouter = express.Router();
 
-// Route to create a new user
-router.post("/users", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password });
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
-// Route to fetch all users
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-export default router;
+export default userRouter;
