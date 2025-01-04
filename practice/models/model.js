87972782e20228bd0f34fb51c9942b69528f0b4d
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-04 09:54:48 +0530

    ..

--- practice/models/model.js
+++ practice/models/model.js
@@ -1,12 +1,9 @@
+import mongoose from "mongoose";
 
-const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
 
-const postSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Post", postSchema);
+export const Post = mongoose.model("Post", postSchema);
