132223d3834ef15fe95c8d4772cebcbbc700699b
urvi1622005 <urvashidhawan1622005@gmail.com>
2025-01-05 19:55:24 +0530

    ..

--- practice/models/model.js
+++ practice/models/model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  category: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    category: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true } // Automatically adds `createdAt` and `updatedAt` fields
+);
 
 export const Post = mongoose.model("Post", postSchema);
